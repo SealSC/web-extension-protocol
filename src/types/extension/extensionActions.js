d6e63672303c2e8d90578f8983cd01ac028153d3
AKACoder <akacoder@outlook.com>
2019-05-22 16:47:23 +0800

    delete some invild methods of ExtensionActions
    Signed-off-by: AKACoder <akacoder@outlook.com>

--- src/types/extension/extensionActions.js
+++ src/types/extension/extensionActions.js
@@ -5,8 +5,6 @@ class ExtensionActions {
   constructor(extension) {
     this.extension = extension
   }
-  async contractOffChainCall() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
-  async invokeContract() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
   async loadContract() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
   async transfer() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
   async transferToken() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
