61f8742b1ed688561bc23abd01736dd2bda13db7
AKACoder <akacoder@outlook.com>
2019-05-24 14:36:19 +0800

    add param placeholders.
    Signed-off-by: AKACoder <akacoder@outlook.com>

--- src/types/extension/extensionActions.js
+++ src/types/extension/extensionActions.js
@@ -5,9 +5,10 @@ class ExtensionActions {
   constructor(extension) {
     this.extension = extension
   }
-  async loadContract() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
-  async transfer() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
-  async transferToken() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
+
+  async loadContract(abi, address) {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
+  async transfer(to, amount, memo, extra) {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
+  async transferToken(wrapper, to, amount, extra) {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
   async getAccount() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
 }
 
--- src/types/extension/extensionContractCaller.js
+++ src/types/extension/extensionContractCaller.js
@@ -6,8 +6,8 @@ class ExtensionContractCaller {
     this.extension = extension
   }
 
-  async onChainCall() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
-  async offChainCall() {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
+  async onChainCall(wrapper, method, param, amount, extra) {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
+  async offChainCall(wrapper, method, param) {return new Result(null, predefinedStatus.NOT_SUPPORT(null))}
 }
 
 export {
